5fa6fc3dd9ca68f12c54c097bcdbc192639d10e3
fidelineBazana <fideline.bazana@ooeo.fr>
2022-01-03 12:15:18 +0100

    structure

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+import pygame
 
-class player:
-  def __init__(self):
-    self.pos = (2,2)
-  def attack(self):
-    print("attack")
+
+module_charge = pygame.init ()
+print (module_charge)
+
+ecran = pygame.display.set_mode((500,500))
+pygame.display.set_caption("Le jeu du pendu")
+#image = pygame.image.load("logo.png").convert()
+pygame.display.set_icon("logo.jpeg")
+
+#print (image)
+
+
+
+#vide le cache
+pygame.quit()
