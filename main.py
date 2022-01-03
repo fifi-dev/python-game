8091768ee2cf7e3c0bb87ba4a012a8294d444422
“Fideline <fideline.bazana_ntomo@edu.devinci.fr>
2022-01-03 09:43:21 +0100

    password validator

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-x = int(input("Saisir une valeur: "))
-y = int(input("Saisir une deuxième valeur: "))
 
-z = x+y 
-print(z)
+password = "123"
+EnterPassword = input("Saisir le mot de passe: ")
+
+if (password == EnterPassword):
+
+  print("Vous etes connecté")
+
+else:
+  print("mot de passe incorrect")
